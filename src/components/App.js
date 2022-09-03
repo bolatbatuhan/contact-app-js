5ba582783cff426d0f8506028cdc9ed9e39cc54d
Batuhan Bolat <bolatbatuhan@gmail.com>
2022-09-03 22:23:40 +0300

    contact

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts"
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact])
+  }
+
+  useEffect(() => {
+   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+   if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+
+  return (
+    <div className='ui container'>
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts= {contacts} />
+
+    </div>
+  );
+}
+
+export default App;
